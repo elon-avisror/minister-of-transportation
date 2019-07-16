1bef92696e54ec4653a99e4a3e111d66fde8588d
elonavisrur <elavis2@gmail.com>
2019-07-16 23:28:41 +0300

    initial abstract project

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-import { ethers } from "ethers";
+/*
+import Web3 from 'web3';
+import {Eth} from 'web3-eth';
+
+// "Web3.givenProvider" will be set if in an Ethereum supported browser.
+const eth = new Eth(Web3.givenProvider || 'ws://some.local-or-remote.node:8546', null, options);
+
+web3.eth.getAccounts(console.log);
+*/
 
 issueLicense = () => {
   console.log("issueLicense");
--- smart_contract/mot.sol
+++ smart_contract/mot.sol
@@ -1,10 +1,23 @@
-pragma solidity >=0.4.17 <0.6.0;
+pragma solidity ^0.5.1;
 
-contract mot {
+contract MinisterOfTransportatoion  {
+    mapping(uint => Driver) public drivers;
+    address owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     struct Driver {
-        uint personID;
-        address licenseID;
-        bool isExpiry;
+        uint32 id;
+        string name;
+    }
+    event Add(uint32 _id, string _name);
+    constructor() public {
+        owner = msg.sender;
+    }
+    function addDriver(uint32 _id, string memory _name) public onlyOwner {
+        drivers[_id] = Driver(_id, _name);
+        emit Add(_id, _name);
     }
 }
